75fcc5d200fc1c0e6bbf51b7b90c3ed3fa26a9ac
Gabriela <gmorales1424@gmail.com>
2020-10-11 07:31:59 -0300

    Login normal seccion 10

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,22 @@
 // Puerto
 //*************************** */
 process.env.PORT = process.env.PORT || 3000;
+
+
+//*************************** */
+// Entorno
+//*************************** */
+process.env.NODE_ENV = process.env.MODE_ENV || 'dev';
+
+//*************************** */
+// Vencimiento del Token
+//*************************** */
+
+//así expira en 30 dias - 60 * 60 es 1 hora, por 24, por 30
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
+
+//*************************** */
+// SEED de autenticación
+//*************************** */
+process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo';
--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 require('./config/config');
 
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 const bodyParser = require('body-parser');
 
@@ -10,50 +12,20 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-app.get('/usuario', function(req, res) {
-    res.json('get Usuario');
+//configuracion global de rutas
+app.use(require('./routes/index'));
 
-})
-
-app.post('/usuario', function(req, res) {
-
-    let body = req.body;
-
-    // res.json({
-    //     persona: body
-    // });
-
-    if (body.nombre === undefined) {
-
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-
-    } else {
-
-        res.json({
-            persona: body
-        });
-
-    }
+mongoose.connect('mongodb://localhost:27017/cafe', { useNewUrlParser: true, useCreateIndex: true },
+    (err, res) => {
 
 
+        if (err) throw err;
 
-})
+        console.log('BD ONLINE');
 
-app.put('/usuario/:id', function(req, res) {
-
-    let id = req.params.id;
-
-    res.json({
-        id
     });
-})
 
-app.delete('/usuario', function(req, res) {
-    res.json('delete Usuario');
-})
+
 
 app.listen(process.env.PORT, () => {
     console.log('Escuchando el puerto:', process.env.PORT);
